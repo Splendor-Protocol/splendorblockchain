c621209658706be1dadaebbd1c0fea6c106959cd
stardust13e8 <>
2025-12-28 00:09:59 +0530

    major updates

--- Core-Blockchain/node_src/consensus/congress/systemcontract/address_list.go
+++ Core-Blockchain/node_src/consensus/congress/systemcontract/address_list.go
@@ -14,8 +14,8 @@ import (
 )
 
 var (
-	devAdmin        = common.HexToAddress("0x6a0e7ae8eded108a4ec0e88d355d9279fad93fce")
-	devAdminTestnet = common.HexToAddress("0x0dA5ac74D30D5b3c5ca9167A8666Ca98Fd58d9fb")
+	devAdmin        = common.HexToAddress("0xF5BBDF432EcCCeF7eD8E96D643edB26D76390C84")
+	devAdminTestnet = common.HexToAddress("0xF5BBDF432EcCCeF7eD8E96D643edB26D76390C84")
 )
 
 const (
