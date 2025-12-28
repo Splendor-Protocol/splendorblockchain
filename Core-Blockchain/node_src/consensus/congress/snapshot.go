2af44467315bb331c759ebec5806ad7a6277359f
stardust13e8 <>
2025-12-29 02:25:57 +0530

    bug fixes

--- Core-Blockchain/node_src/consensus/congress/snapshot.go
+++ Core-Blockchain/node_src/consensus/congress/snapshot.go
@@ -19,14 +19,12 @@ package congress
 import (
 	"bytes"
 	"encoding/json"
-	"math"
 	"sort"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 	lru "github.com/hashicorp/golang-lru"
 )
